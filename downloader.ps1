c1b7f55163055c2cf28c0dfa2796cac59169295f
Richard Donnell <richardadonnell@gmail.com>
2023-09-11 05:22:08 -0500

    download 5 files in parallel

--- downloader.ps1
+++ downloader.ps1
@@ -1,3 +1,5 @@
+# TODO: Make the code download 5 files in parallel
+
 # Define the path to your text file
 $scannedURLsPath = ".\Scans\http___www.exploremarmaris.com_read_Survival_.txt"
 $outputPath = ".\Output"
@@ -6,25 +8,43 @@ $outputPath = ".\Output"
 $urls = Get-Content $scannedURLsPath
 
 # Loop through each URL and download the file
+$jobCount = 0
 foreach ($url in $urls) {
     $url = $url.Trim()  # Remove any leading/trailing spaces
-    
+
     # Extract the filename from the URL
     $fileName = [System.IO.Path]::GetFileName($url)
-    
+
     # Decode the URL-encoded file name
     $fileName = [System.Web.HttpUtility]::UrlDecode($fileName)
-    
-    # Use the Invoke-WebRequest cmdlet to download the file
-    try {
-        Invoke-WebRequest -Uri $url -OutFile (Join-Path -Path $outputPath -ChildPath $fileName) -ErrorAction Stop
-        Write-Host "Downloaded $fileName"
+
+    # Use Start-Job cmdlet to run the download operation in the background
+    $job = Start-Job -ScriptBlock {
+        param($url, $outputPath, $fileName)
+        try {
+            Invoke-WebRequest -Uri $url -OutFile (Join-Path -Path $outputPath -ChildPath $fileName) -ErrorAction Stop
+            Write-Host "Downloaded $fileName"
+        }
+        catch {
+            $errorMessage = $_.Exception.Message
+            Write-Host "Failed to download $fileName. Error: $errorMessage"
+            Write-Error $errorMessage  # Add error logging
+        }
+    } -ArgumentList $url, $outputPath, $fileName
+
+    # Keep track of the number of running jobs
+    $jobCount++
+
+    # Wait for the number of running jobs to reach the desired parallelism (5 in this case)
+    if ($jobCount -ge 5) {
+        # Wait for all jobs to complete before continuing
+        Get-Job | Wait-Job | Receive-Job
+        $jobCount = 0
     }
-    catch {
-        $errorMessage = $_.Exception.Message
-        Write-Host "Failed to download $fileName. Error: $errorMessage"
-        Write-Error $errorMessage  # Add error logging
-    }
-    
-    Start-Sleep -Seconds 0.1  # Wait for X seconds before the next request
 }
+
+# Wait for any remaining jobs to complete
+Get-Job | Wait-Job | Receive-Job
+
+# Clean up finished jobs
+Get-Job | Remove-Job
